206d228e0c9c86c43d8526e3aef6c53be7d13dbd
Fernando Caique Soares <fndcaique@gmail.com>
2020-11-26 19:06:42 -0300

    refactor description of test 6

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -87,9 +87,8 @@ describe('#createMenu', () => {
     // ```
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
-    // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens no array `objetoRetornado.consumption` conforme os itens pedidos.
+    // TESTE 6: Verifique que as três orders seguintes, de bebidas e comidas mescladas, somam três itens (além da coxinha que foi adicionada anteriormente) ao array `objetoRetornado.consumption` conforme os itens pedidos.
     // ```
-    // objetoRetornado.order("coxinha");
     // objetoRetornado.order("agua");
     // objetoRetornado.order("sopa");
     // objetoRetornado.order("sashimi");
